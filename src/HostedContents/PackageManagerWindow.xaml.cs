84d7c79645479715580bc42209072bf7a4951b7a
Ben Goh <benglin@outlook.com>
2017-01-11 12:28:57 +0800

    Initial commit for 'HostedContents' project

---
+++ src/HostedContents/PackageManagerWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace HostedContents
+{
+    /// <summary>
+    /// Interaction logic for PackageManagerWindow.xaml
+    /// </summary>
+    public partial class PackageManagerWindow : Window
+    {
+        public PackageManagerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
