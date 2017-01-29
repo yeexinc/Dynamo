016bcfa1f49c446c098c232d7b312734edb4582d
Ben Goh <benglin@outlook.com>
2017-01-30 00:38:09 +0800

    Introduced a new 'LibraryContainer' component

---
+++ src/HostedContents/src/components/LibraryContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+declare var boundContainer: any;
+
+export interface LibraryContainerProps { }
+
+export class LibraryContainer extends React.Component<LibraryContainerProps, undefined> {
+    render() {
+        return (<div></div>);
+    }
+}
