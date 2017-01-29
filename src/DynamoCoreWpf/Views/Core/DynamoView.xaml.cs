e52da2e51d4b15c5afe19f2baf9449a70f68d5d2
Ben Goh <benglin@outlook.com>
2017-01-30 01:18:46 +0800

    Displayed child library items

--- src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
+++ src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
@@ -525,10 +525,10 @@ namespace Dynamo.Controls
                 var result = assembly.CreateInstance("Dynamo.HostedContents.LibraryContainer");
                 sidebarGrid.Children.Add(result as UserControl);
 
-                var loadedTypesJson = new StringBuilder("{ childNodes: [");
+                var loadedTypesJson = new StringBuilder("{ \"childNodes\": [");
                 foreach (var entry in dynamoViewModel.Model.SearchModel.SearchEntries)
                 {
-                    loadedTypesJson.Append(string.Format(" { name: '{0}' },", entry.Name));
+                    loadedTypesJson.Append(string.Format(" {{ \"name\": \"{0}\" }},", entry.Name));
                 }
 
                 loadedTypesJson.Remove(loadedTypesJson.Length - 1, 1);
--- src/HostedContents/src/components/LibraryContainer.tsx
+++ src/HostedContents/src/components/LibraryContainer.tsx
@@ -6,6 +6,19 @@ export interface LibraryContainerProps { }
 
 export class LibraryContainer extends React.Component<LibraryContainerProps, undefined> {
     render() {
-        return (<div></div>);
+
+        try {
+            const rootNode = JSON.parse(boundContainer.getLoadedTypesJson());
+            const childNodes = rootNode.childNodes;
+            const listItems = childNodes.map((childNode : any) => 
+                <div>{ childNode.name }</div>
+            );
+
+            return (<div><div>Hey: </div>{ listItems }</div>);
+        }
+        catch(exception) {
+            return (<div>{ exception.message }</div>);
+        }
+
     }
 }
