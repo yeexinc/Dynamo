56fd420f41e36cb3f54ec3333e4398bcdd94ebe6
Ben Goh <benglin@outlook.com>
2017-01-29 23:27:04 +0800

    Added interface event 'WebBrowserLoaded'

--- src/DynamoCoreWpf/Interfaces/ILibraryContainer.cs
+++ src/DynamoCoreWpf/Interfaces/ILibraryContainer.cs
@@ -1,10 +1,27 @@
 ﻿
+using System;
 using System.Collections.Generic;
 
 namespace Dynamo.Wpf.Interfaces
 {
+    /// <summary>
+    /// An interface to be implemented by the actual library container control. Through
+    /// the implementation of this interface, Dynamo view interacts with the underlying
+    /// library container object.
+    /// </summary>
     public interface ILibraryContainer
     {
+        /// <summary>
+        /// This event is raised when the underlying web browser has completely loaded.
+        /// After this event it will be safe to invoke methods that call into JavaScript.
+        /// </summary>
+        event EventHandler WebBrowserLoaded;
+
+        /// <summary>
+        /// Dynamo view calls this method to pass on loaded types to the library 
+        /// container control.
+        /// </summary>
+        /// <param name="typeNames">A list of type names to show.</param>
         void OnLibraryDataPopulated(IEnumerable<string> typeNames);
     }
 }
--- src/HostedContents/LibraryContainer.xaml.cs
+++ src/HostedContents/LibraryContainer.xaml.cs
@@ -1,7 +1,9 @@
 ﻿
+using CefSharp;
 using Dynamo.Wpf.Interfaces;
-using System.Windows.Controls;
+using System;
 using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace Dynamo.HostedContents
 {
@@ -10,13 +12,54 @@ namespace Dynamo.HostedContents
     /// </summary>
     public partial class LibraryContainer : UserControl, ILibraryContainer
     {
+        private bool browserLoaded = false;
+
         public LibraryContainer()
         {
             InitializeComponent();
+            webBrowser.RegisterJsObject("boundContainer", this);
+            webBrowser.FrameLoadEnd += OnWebBrowserFrameLoadEnd;
         }
+
+        #region Public ILibraryContainer Members
+
+        public event EventHandler WebBrowserLoaded;
 
         public void OnLibraryDataPopulated(IEnumerable<string> typeNames)
         {
+            if (!browserLoaded) return; // Not ready for call right now.
+            SetTypeNames(typeNames);
         }
+
+        #endregion
+
+        #region Gateway Methods: from .NET to JavaScript (private methods)
+
+        private void SetTypeNames(IEnumerable<string> typeNames)
+        {
+            var mainFrame = webBrowser.GetMainFrame();
+            if (mainFrame == null) return;
+
+            mainFrame.ExecuteJavaScriptAsync(@"sendAlertNative('Hahaha');");
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void OnWebBrowserFrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            browserLoaded = true;
+
+            var loadEventHandler = WebBrowserLoaded;
+            if (loadEventHandler != null) loadEventHandler(this, new EventArgs());
+
+            if (e.Frame.IsMain)
+            {
+
+            }
+        }
+
+        #endregion
     }
 }
