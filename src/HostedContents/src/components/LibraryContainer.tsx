6ea912dd86299faef5c64c84c0b0f3dc33ade546
Ben Goh <benglin@outlook.com>
2017-01-30 15:20:27 +0800

    Added item schema for reference

--- src/HostedContents/src/components/LibraryContainer.tsx
+++ src/HostedContents/src/components/LibraryContainer.tsx
@@ -1,6 +1,44 @@
 import * as React from "react";
 import { LibraryItem } from "./LibraryItem";
 
+let itemSchema = 
+{
+    text: "Geometry",
+    iconName: "GeometryCategory.png",
+    expanded: true,
+    itemTyep: "category",
+    childItems: [
+        {
+            text: "Abstract", // Group
+            iconName: "AbstractGroup.png",
+            expanded: true,
+            itemType: "group",
+            childItems: [
+                {
+                    text: "BoundingBox", // Class
+                    iconName: "Autodesk.DesignScript.Geometry.BoundingBox.png",
+                    expanded: true,
+                    itemType: "none",
+                    childItems: [
+                        {
+                            text: "ByCorners", // Method
+                            iconName: "Autodesk.DesignScript.Geometry.BoundingBox.ByCorners.png",
+                            expanded: false,
+                            itemType: "none"
+                        },
+                        {
+                            text: "Intersects", // Method
+                            iconName: "Autodesk.DesignScript.Geometry.BoundingBox.Intersects.png",
+                            expanded: false,
+                            itemType: "none"
+                        },
+                    ]
+                },
+            ]
+        },
+    ]
+};
+
 declare var boundContainer: any;
 
 export interface LibraryContainerProps { }
