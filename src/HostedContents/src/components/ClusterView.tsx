67bd8a3140008a8af30816a6b969a3dd99273256
Ben Goh <benglin@outlook.com>
2017-01-31 17:24:36 +0800

    Initial implementation of 'ClusterView'

--- src/HostedContents/src/components/ClusterView.tsx
+++ src/HostedContents/src/components/ClusterView.tsx
@@ -18,13 +18,32 @@
 */
 
 import * as React from "react";
+import { LibraryItem, ItemData } from "./LibraryItem";
 
-export interface ClusterViewProps { iconPath: string }
+export interface ClusterViewProps { iconPath: string, childItems: ItemData[] }
 
 export class ClusterView extends React.Component<ClusterViewProps, undefined> {
 
     render() {
-        return (<div></div>);
+        return (
+            <div className={ "ClusterViewContainer" }>
+                <div className={ "ClusterLeftPane" }>
+                    <div className={ "ClusterIcon" }>
+                        test
+                    </div>
+                </div>
+                <div className={ "ClusterRightPane" }>
+                    { this.getNestedElements() }
+                </div>
+            </div>
+        );
+    }
+
+    getNestedElements() {
+
+        return this.props.childItems.map((item: ItemData) => {
+            return (<LibraryItem data={ item } />);
+        });
     }
 
 }
