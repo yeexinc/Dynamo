2cdfadb040769d354d32b4f2e37160fb00e0ed7e
Ben Goh <benglin@outlook.com>
2017-02-05 15:10:56 +0800

    Added 'loadedTypes' endpoint

--- src/HostedContents/index.js
+++ src/HostedContents/index.js
@@ -1,12 +1,25 @@
 
-var express = require('express');
+var fs = require("fs");
+var express = require("express");
 var app = express();
 app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   res.render("index.html");
-})
+});
+
+app.get("/loadedTypes", function(req, res) {
+  fs.readFile("./docs/RawTypeData.json", function(err, data) {
+    if (err) {
+      res.end(err.message);
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data.toString());
+    }
+  });
+
+});
 
 app.listen(3456, function () {
   console.log('Library hosting service listening on port 3456');
-})
+});
