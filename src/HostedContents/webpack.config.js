9b3346bf0e59e4b6d9c572455da1d080ed9064d7
Ben Goh <benglin@outlook.com>
2017-02-07 21:29:41 +0800

    Updated webpack configuration to version 2.0

--- src/HostedContents/webpack.config.js
+++ src/HostedContents/webpack.config.js
@@ -13,20 +13,25 @@ module.exports = {
     devtool: "source-map",
 
     resolve: {
+        modules: [
+            "node_modules"
+        ],
+
         // Add '.ts' and '.tsx' as resolvable extensions.
-        modulesDirectories: ["node_modules"],
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: [ ".webpack.js", ".web.js", ".ts", ".tsx", ".js" ]
     },
 
     module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader" // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            },
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader" // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            }
         ]
     },
 
